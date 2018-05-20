ee1084aca49b274def6e97f01c6612433584debc
Brian Weyer <brianweyer@gmail.com>
2018-05-19 19:34:03 -0500

    Update modules to include the routes file

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
+import { routes } from 'src/app/routes';
 import { LoginComponent } from 'src/app/views/login/login.component';
 import { KioskComponent } from 'src/app/views/kiosk/kiosk.component';
 import { OnTapComponent } from 'src/app/views/on-tap/on-tap.component';
@@ -9,7 +10,7 @@ import { BreweriesComponent } from 'src/app/views/breweries/breweries.component'
 import { HistoryComponent } from 'src/app/views/history/history.component';
 import { NotFoundComponent } from 'src/app/views/not-found/not-found.component';
 
-const appRoutes: ModuleWithProviders = RouterModule.forRoot([], { enableTracing: true });
+const appRoutes: ModuleWithProviders = RouterModule.forRoot(routes, { enableTracing: true });
 
 @NgModule({
     imports: [
